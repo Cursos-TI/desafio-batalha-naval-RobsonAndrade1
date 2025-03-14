a1bb62c427f3e4af8d6e83238d3e6ac336479772
Robson Andrade <113925056+RobsonAndrade1@users.noreply.github.com>
2025-03-14 02:38:24 +0000

    Navios Diagonais

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -13,7 +13,7 @@ int main() {
         }
     }
 
- // Posicionando navio horizontal (ocupa 3 posições)
+// Posicionando navio horizontal (ocupa 3 posições)
     int navio_horizontal[3] = {2, 3, 4};  // Coloca o navio nas colunas 2, 3 e 4 na linha 2
     for (int i = 0; i < 3; i++) {
         tabuleiro[2][navio_horizontal[i]] = 3;  // Marca as posições do navio com 3
@@ -23,6 +23,22 @@ int main() {
     int navio_vertical[3] = {3, 4, 5};  // Coloca o navio nas linhas 3, 4 e 5 na coluna 7
     for (int i = 0; i < 3; i++) {
         tabuleiro[navio_vertical[i]][7] = 3;  // Marca as posições do navio com 3
+    }
+
+// Posicionando navio diagonal (da esquerda superior para a direita inferior)
+    int navio_diagonal1[3] = {2, 3, 4};  // Linha e coluna aumentam
+    for (int i = 0; i < 3; i++) {
+        if (tabuleiro[navio_diagonal1[i]][navio_diagonal1[i] - 2] == 0) {
+            tabuleiro[navio_diagonal1[i]][navio_diagonal1[i] - 2] = 3;  // Marca as posições do navio com 3
+        }
+    }
+
+// Posicionando navio diagonal (da direita superior para a esquerda inferior)
+    int navio_diagonal2[3] = {0, 1, 2};  // Linha aumenta e coluna diminui
+    for (int i = 0; i < 3; i++) {
+        if (tabuleiro[navio_diagonal2[i]][9 - navio_diagonal2[i]] == 0) {
+            tabuleiro[navio_diagonal2[i]][9 - navio_diagonal2[i]] = 3;  // Marca as posições do navio com 3
+        }
     }
 
 // Exibindo o tabuleiro
